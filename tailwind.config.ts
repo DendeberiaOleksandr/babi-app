c8b9f99d1b59aaab0fb4e4d62e5b8e2ef72446bb
DendeberiaOleksandr <oleksandr.dendeberia@gmail.com>
2023-11-22 01:31:15 +0100

    login page template

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+      colors: {
+        'primary': '#F56EB3',
+        'pink1': '#CB1C8D',
+        'pink2': '#7F167F',
+        'black': '#460C68',
+      }
+    }
   },
   plugins: [],
 }
